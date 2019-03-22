42090cb0cffefba8a3171505b79a0045bc69ac7d
Sahil Singh <sahilsingh.dsc@gmail.com>
2019-03-22 18:03:09 +0530

    Padding Margin @ 22/03/19 18:02

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,10 @@
-import 'package:first_flutter_app/app_screens/first_screen.dart';
+import 'package:first_flutter_app/app-screens/home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyFlutterApp());
-
-class MyFlutterApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-    return  MaterialApp(
-
-        debugShowCheckedModeBanner: false,
-        title: "Hello Flutter App",
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("App Screen"),
-          ),
-          body: FirstScreen()
-        )
-    );
-  }
-
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Exploring UI widgets",
+    home: Home(),
+  ));
 }
